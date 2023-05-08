76c68bd1818a28c8bd0848062aad0febfd5372d4
Leonel Moran <leonelzecena2020@gmail.com>
2023-05-08 11:26:14 -0600

    Boton para subir hacía arriba

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose) => {
 
 var swiper = new Swiper(".portfolio__container", {
     cssMode: true,
-    loop:true,
+    loop: true,
 
     navigation: {
         nextEl: ".swiper-button-next",
@@ -103,17 +103,47 @@ var swiper = new Swiper(".portfolio__container", {
     },
     pagination: {
         el: ".swiper-pagination",
-        clickable:true,
+        clickable: true,
     },
 });
 
 /*==================== SECCIONES DE DESPLAZAMIENTO ENLACE ACTIVO ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CAMBIAR EL FONDO DE LA CABECERA ====================*/
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== MOSTRAR DESPLAZAMIENTO HACIA ARRIBA ====================*/
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
 
 
 /*==================== DARK LIGHT TEMA ====================*/ 
